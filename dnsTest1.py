71e4b38a1f91881fbcc4dcb982ef418e52a30756
cs168 <xguo@berkeley.edu>
2014-11-13 16:44:32 -0800

    upadate dnastest 1

--- dnsTest1.py
+++ dnsTest1.py
@@ -10,13 +10,9 @@ def ip_int_to_str(ipNum):
 	ipStrList.append((ipNum >> 0) & 255)
 	return "%d.%d.%d.%d" % (ipStrList[0], ipStrList[1], ipStrList[2], ipStrList[3])
 
-f = open ('dnspacketpool0')
+f = open ('tcppacketpool0')
 inputbuffer = f.read()
 firewallTest = Firewall(None, None, None)
 
-archieve = firewallTest.packet_allocator(PKT_DIR_OUTGOING, inputbuffer)
-print 'external ip: ' + ip_int_to_str(archieve.externalIP)
-print 'protocol: ' + archieve.protocol
-print 'externalPort: ' + str(archieve.externalPort)
-print 'self.domainName: ' + archieve.domainName
-
+archieve = firewallTest.packet_allocator(PKT_DIR_OUTGOING, inputbuffer, firewallTest.countryCodeDict)
+print(archieve)
